4f941e207376a974e7489a6a78b086774cd0d154
Alexander Besse <alexanderjoeb@hotmail.com>
2020-07-23 18:24:11 -0500

    Single Number Completed.

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -3,9 +3,25 @@ Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
 def single_number(arr):
-    # Your code here
+    
+    # Initalize dictionary to how the amount of times an iteger is seen.
+    elems = {}
 
-    pass
+    # Enumerate through the array of numbers.
+    for index, item in enumerate(arr):
+        if item not in elems:
+            elems[item] = 1
+        else:
+            elems[item] += 1
+        
+    # Loop through all the key and value pairs in `elems`. 
+    # Find the key that has the value of 1.
+    for key, value in elems.items():
+        if value == 1:
+            return key
+        else: continue
+
+    return None
 
 
 if __name__ == '__main__':
