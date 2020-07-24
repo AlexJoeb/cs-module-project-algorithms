180a8a46fb34ed186b7ec725cb0bf34c5eda9f5f
Alexander Besse <alexanderjoeb@hotmail.com>
2020-07-23 19:02:45 -0500

    Made modifications to RPS comments.

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,7 +2,6 @@
 
 import sys
 
-
 def rock_paper_scissors(n):
   # Write a function `rock_paper_scissors` to generate all of the possible plays that can be made in a game of "Rock Paper Scissors", 
   # given some input `n`, which represents the number of plays per round. 
@@ -25,39 +24,9 @@ def rock_paper_scissors(n):
     
     for index, option in enumerate(options):
       helper(cache + [option], counter - 1)
-    
-
 
   helper([], n)
   return possibles
-
-
-    
-
-  #  * Another problem that asks you to generate a bunch of permutations, so we're probably going to want to opt for using recursion again. 
-  #     Since we're building up a list of results, we'll have to pass the list we're constructing around to multiple recursive calls so that
-  #     each recursive call can add to the overall result. However, the tests only give our function `n` as input. To get around this, 
-  #     we could define an inner recursive helper function that will perform the recursion for us, while allowing us to preserve the outer 
-  #     function's function signature. 
-  #  * In Python, you can concatenate two lists with the `+` operator. However, you'll want to make sure that both operands are lists!
-  #  * If you opt to define an inner recursive helper function, don't forget to make an initial call to the recursive helper function to kick off the recursion.
-
-#   var roundChoice = function(cache, counter) {
-#     for(var i = 0; i < options.length; i++){
-#       cache.push(options[i]);
-#       if(counter === n){
-#         possibles.push(cache.slice());
-#       }else{
-#         roundChoice(cache, counter + 1);
-#       }
-#       cache.pop();
-#      }
-#    }
-#   roundChoice([], 1);
-#   return allPossibilities;
-# }
-
-  pass
 
 
 if __name__ == "__main__":
